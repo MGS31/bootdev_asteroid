10c774ad4c529f31ba486124fbdccce336ab4f84
Matt <mgstankov30@gmail.com>
2026-05-05 13:03:41 -0400

    create asteroids group, add the Asteroid object to this, updatable and drawable) create an asteroidfields contaier for updatable only. Invoke asteroidfield so it starts to draw it

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     screen= pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -11,10 +13,14 @@ def main():
     clock = pygame.time.Clock()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     dt = 0
     
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    AsteroidField()
 
     while True:
         log_state()
