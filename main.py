0b0e2f60723d5520471c7dd68f8b982e9156d70d
Matt <mgstankov30@gmail.com>
2026-05-05 11:53:13 -0400

    add player class initialization and rendering of player on screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     screen= pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -8,6 +9,7 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\n Screen width: {SCREEN_WIDTH}\n Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -16,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
