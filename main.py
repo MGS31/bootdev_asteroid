10af167b5da6388c3649ea1704b00f3a4655806d
Matt <mgstankov30@gmail.com>
2026-05-05 11:24:50 -0400

    update the initialization to use gameclock to limit frame usage

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from logger import log_state
 
 def main():
     screen= pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\n Screen width: {SCREEN_WIDTH}\n Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         log_state()
@@ -14,6 +17,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
