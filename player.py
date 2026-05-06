7ddda82580b01804783fcbd05866c6097f43ad3a
Matt <mgstankov30@gmail.com>
2026-05-06 10:47:02 -0400

    add new variable for tracking cooldown time and validate if the cooldown is less than or equal to 0 before allowing a shot.

--- player.py
+++ player.py
@@ -1,12 +1,13 @@
 import pygame
 from circleshape import CircleShape
 from shot import Shot
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, SHOT_RADIUS, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, SHOT_RADIUS, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 
 class Player(CircleShape):
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.rotation = 0
+    self.shot_cooldown = 0
 
   def triangle(self):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -34,7 +35,10 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
            self.move(dt * -1)
         if keys[pygame.K_SPACE]:
-           self.shoot()
+           if self.shot_cooldown <= 0:
+              self.shoot()
+              self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
+
 
   def move(self, dt):
     unit_vector = pygame.math.Vector2(0, 1)
