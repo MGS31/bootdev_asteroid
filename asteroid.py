b4d5b20db33d4e8177e2ea31b7fda1fbf5c62e24
Matt <mgstankov30@gmail.com>
2026-05-06 11:12:11 -0400

    add the split method to make two asteroids if they are big enough to be split

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
   def __init__(self, x, y, radius):
@@ -11,3 +13,18 @@ class Asteroid(CircleShape):
   
   def update(self, dt):
     self.position += (self.velocity * dt)
+  
+  def split(self):
+    pygame.sprite.Sprite.kill(self)
+    if self.radius <= ASTEROID_MIN_RADIUS:
+      return
+    else:
+      log_event("asteroid_split")
+      random_angle = random.uniform(20, 50)
+      rotate_one = pygame.math.Vector2.rotate(self.velocity, random_angle)
+      rotate_two = pygame.math.Vector2.rotate(self.velocity, -random_angle)
+      new_radius = self.radius - ASTEROID_MIN_RADIUS
+      first_new = Asteroid(self.position.x, self.position.y, new_radius)
+      second_new = Asteroid(self.position.x, self.position.y, new_radius)
+      first_new.velocity = rotate_one
+      second_new.velocity = rotate_two
